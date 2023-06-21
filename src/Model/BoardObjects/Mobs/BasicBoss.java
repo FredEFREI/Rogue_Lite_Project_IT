7b1f35aae499e1d52e1403704f715a6347021c5e
Rockmate77 <52208570+Rockmate77@users.noreply.github.com>
2023-06-21 12:17:50 +0200

    création d'un boss

--- src/Model/BoardObjects/Mobs/BasicBoss.java
+++ src/Model/BoardObjects/Mobs/BasicBoss.java
@@ -7,61 +7,60 @@ import Model.BoardObjects.Empty;
 import Model.BoardObjects.ObjType;
 
 import java.awt.*;
-import java.util.ArrayList;
 
-public class BasicEnemy extends BoardObject implements Mob{
+public class BasicBoss extends BoardObject implements Mob {
     int health = 100;
-    public BasicEnemy(){
+
+    public BasicBoss() {
         type = ObjType.enemy;
         Dimension d = Board.generateCoordinates(this);
         boardX = d.width;
         boardY = d.height;
     }
 
-    public BasicEnemy(Dimension coordinates){
+    public BasicBoss(Dimension coordinates) {
         type = ObjType.enemy;
-        boardX=coordinates.width;
-        boardY=coordinates.height;
+        boardX = coordinates.width;
+        boardY = coordinates.height;
     }
 
     @Override
     public boolean isOnBoard() {
-        if(boardY>=0 && boardX>=0)
+        if (boardY >= 0 && boardX >= 0)
             return true;
         return false;
     }
 
     public void attack(Mob m) {
-        int damages = (int)  Math.round(Math.random() * 15 *(1+Board.getBossDefeated()));
+        int damages = (int) Math.round(Math.random() * 35 * (1 + Board.getBossDefeated()));
         m.inflictDamage(damages);
-        System.out.println(this.getClass().getSimpleName()+" inflicted "+damages+" of damage to "+m.getClass().getSimpleName());
+        System.out.println(this.getClass().getSimpleName() + " inflicted " + damages + " of damage to " + m.getClass().getSimpleName());
     }
 
     public void die() {
-        health=0;
+        health = 0;
         System.out.println("Your enemy dropped:");
-        int nbitems=1+(int) Math.round(Math.random()*2);
-        Item it=null;
+        int nbitems = 1 + (int) Math.round(Math.random() * 2);
+        Item it = null;
         for (int i = 0; i < nbitems; i++) {
-            switch ((int) Math.round(Math.random() * 2)) {
+            switch ((int) Math.round(Math.random() * 3)) {
                 case 0:
-                    it=new Armor();
+                    it = new Armor();
                     System.out.println("Armor");
                     break;
                 case 2:
-                    it=new Sword();
+                    it = new Sword();
                     System.out.println("Sword");
                     break;
                 case 1:
                     System.out.println("Steroids:");
-                    it=new Steroids();
+                    it = new Steroids();
                     break;
-                /*case 3:
-                    it=new EngineeringDiploma();
-                    System.out.println("Engineering diploma");
-                    break;*/
             }
-            if(it!=null) {
+            it=new EngineeringDiploma();
+            System.out.println("Engineering diploma");
+            Board.bossDefeat();
+            if (it != null) {
                 Board.getBoard()[it.getBoardX()][it.getBoardY()] = new Empty(new Dimension(it.getBoardX(), it.getBoardY()));
                 it.collect(Board.getPlayer());
             }
@@ -74,16 +73,16 @@ public class BasicEnemy extends BoardObject implements Mob{
 
     @Override
     public void inflictDamage(int damages) {
-        health-=damages;
+        health -= damages/10;
     }
 
     public void setHealth(int i) {
-        health=i;
+        health = i;
     }
 
     @Override
     public boolean isDead() {
-        if(health<=0)
+        if (health <= 0)
             return true;
         return false;
     }
@@ -95,6 +94,6 @@ public class BasicEnemy extends BoardObject implements Mob{
 
     @Override
     protected Dimension getCoordinates() {
-        return new Dimension(boardX,boardY);
+        return new Dimension(boardX, boardY);
     }
 }
